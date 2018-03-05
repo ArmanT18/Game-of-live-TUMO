f3c9574f3c3d8bbd537adf191467a2b75f0488a2
user <user@comp13m.tumo.lab>
2018-03-05 18:39:08 +0400

    start of project

---
+++ script.js
@@ -0,0 +1,122 @@
+var matrix = []
+var side = 10;
+var xotArr = [];
+var eatArr = [];
+var gishArr = [];
+var vorsArr = [];
+
+
+
+var bardz = 100;
+var layn = 100;
+var grassCount = 300;
+var eatgrassCount = 300;
+var gishatichCount = 15;
+var vorsordCount = 20;
+var matrix = [];
+
+for(var i = 0; i < bardz; i++){
+    matrix.push([]);
+    for (var j = 0; j < layn; j++) {
+        matrix[i].push(0);
+    }
+}
+
+
+
+function setup() {
+
+for (var i = 0; i < grassCount; i++) {
+    var x = Math.floor(random(0, layn));
+    var y = Math.floor(random(0, bardz));
+    matrix[y][x] = 1;
+}
+
+for (var i = 0; i < eatgrassCount; i++) {
+    var x = Math.floor(random(0, layn));
+    var y = Math.floor(random(0, bardz));
+    matrix[y][x] = 2;
+}
+
+for (var i = 0; i < gishatichCount; i++) {
+    var x = Math.floor(random(0, layn));
+    var y = Math.floor(random(0, bardz));
+    matrix[y][x] = 3;
+}
+
+for (var i = 0; i < vorsordCount; i++) {
+    var x = Math.floor(random(0, layn));
+    var y = Math.floor(random(0, bardz));
+    matrix[y][x] = 4;
+}
+
+
+
+
+noStroke()
+frameRate(3);
+createCanvas(matrix[0].length * side, matrix.length * side);
+background('#acacac');
+
+
+for (var i = 0; i < matrix.length; i++) {
+for (var j = 0; j < matrix[i].length; j++) {
+   if (matrix[i][j] == 2) {
+     var eatgrass = new Eatgrass(j, i, 2);
+     eatArr.push(eatgrass);
+ } else if (matrix[i][j] == 1) {
+     var grass = new Grass(j, i, 1);
+     xotArr.push(grass);
+ } else if (matrix[i][j] == 3) {
+     var gish = new Gishatich(j, i, 3);
+     gishArr.push(gish);
+ } else if (matrix[i][j] == 4) {
+     var vors = new  Vorsord(j, i, 4);
+     vorsArr.push(vors);
+      }
+   }
+}
+
+}
+
+function draw() {
+background('#acacac');
+for (var i = 0; i < matrix.length; i++) {
+for (var j = 0; j < matrix[i].length; j++) {
+    if (matrix[i][j] == 1) {
+     fill("green");
+     rect(j * side, i * side, side, side);
+  } else if (matrix[i][j] == 2) {
+     fill("orange");
+     rect(j * side, i * side, side, side);
+  } else if (matrix[i][j] == 0) {
+     fill('#acacac');
+     rect(j * side, i * side, side, side);
+  } else if (matrix[i][j] == 3) {
+     fill('red');
+     rect(j * side, i * side, side, side);
+  } else if (matrix[i][j] == 4) {
+     fill('blue');
+     rect(j * side, i * side, side, side);
+      }
+   }
+}
+
+
+
+for (var i in xotArr) {
+xotArr[i].mul();
+}
+
+for (var i in eatArr) {
+eatArr[i].eat();
+}
+
+for (var i in gishArr) {
+gishArr[i].eat();
+   }
+
+for (var i in vorsArr) {
+vorsArr[i].kill();
+   }
+}
